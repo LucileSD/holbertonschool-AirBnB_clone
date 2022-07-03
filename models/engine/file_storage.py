5f986ca68dec8dccf9b6d5bc63342c83b1844626
LucileSD <4316@holbertonschool.com>
2022-07-03 08:35:24 +0000

    tests

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,6 @@
     import modules
 """
 import json
-from textwrap import indent
 from models.base_model import BaseModel
 import os
 from models.user import User
@@ -47,7 +46,7 @@ class FileStorage:
         for key, value in self.__objects.items():
             dict[key] = value.to_dict()
         with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(dict, f, indent=2)
+            json.dump(dict, f)
 
     def reload(self):
         """
